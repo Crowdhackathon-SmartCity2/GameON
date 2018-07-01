ba1cc018241d8f89108626b12e710416a22d4fe6
Nik Sakkas <unsorted01@gmail.com>
2018-07-01 10:30:21 +0300

    first commit

---
+++ GameON_SmartCity/Services/IBattleService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameON_SmartCity.Services
+{
+    public interface IBattleService
+    {
+        Task BookBattleGame();
+
+        Task CancelBattleGame();
+
+        Task InviteToBattleGame();
+
+        Task SendMessageToBattleGame();
+
+    }
+}
